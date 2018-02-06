7d72e9da2b0410d6ca684723a17e47cc1b192a85
Michele Mazzucco <hello@michelemazzucco.it>
2018-02-06 20:15:30 +0100

    update packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'data',
-        path: '${__dirname}/src/data/'
+        path: './src/data/'
       }
     },{
       resolve: 'gatsby-plugin-favicon',
