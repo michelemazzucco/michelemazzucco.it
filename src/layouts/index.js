7622d540d830f3f7a89893b36a8853993ac4d1c8
Michele Mazzucco <hello@michelemazzucco.it>
2018-02-09 20:53:33 +0100

    file linted

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,14 +1,9 @@
 import React from 'react'
 import Navigation from '../components/Navigation'
-import PageTransitionHandler from '../components/PageTransitionHandler'
 import '../utils/globalStyles'
 
-export default props => {
-  console.log(props)
-  return (
-    <main>
-      {props.children()}
-      <Navigation location={props.location}/>
-    </main>      
-  )  
-}
+export default ({ children, location }) =>
+  <main>
+    {children()}
+    <Navigation location={location}/>
+  </main>      
