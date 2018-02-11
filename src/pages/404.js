eaec001ad233a1e10fc85f0d25cbb7769ba086a5
Michele Mazzucco <hello@michelemazzucco.it>
2018-02-11 08:52:56 +0100

    added 404 and deploy script

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import styled from 'styled-components'
+import { fonts, colors, media } from '../utils/commonStyles'
 
-const NotFound = () =>
-  <div>
-    <h1>Not found</h1>
-  </div>
+const NotFoundTitle = styled.h1`
+  font-size: 2.2rem;
+  margin-bottom: 1.5rem;
+  text-align: center;
+`
 
-export default NotFound
+const NotFoundSubtitle = styled.h1`
+  text-align: center;
+  font-size: 1.1rem;
+  font-family: ${fonts.mono};
+  color: ${colors.gray500};
+  
+  span {
+    color: ${colors.yellow500};
+  }
+`
+
+const NotFoundWrapper = styled.section`
+  position: absolute;
+  top: 50%;
+  left: 0;
+  right: 0;
+  transform: translateY(-50%);
+  ${media.sm`
+    top: 45%;
+  `}
+`
+
+export default ({ location }) =>
+  <NotFoundWrapper>
+    <header>
+      <NotFoundTitle>Oops!</NotFoundTitle>
+      <NotFoundSubtitle>
+        <span>{location.pathname}</span>
+        &nbsp;page cannot be found!
+      </NotFoundSubtitle>
+    </header>
+  </NotFoundWrapper>
