4658b8a13792da840c1cf9cca87821ab8951a071
Michele Mazzucco <hello@michelemazzucco.it>
2018-03-22 21:53:00 +0100

    wip on deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
-
 /* eslint-disable */
 require('dotenv').config()
 const fs =require('fs')
-/* eslint-disable */
 const AWS = require('aws-sdk')
 const s3 = require('s3')
 const chalk = require('chalk')
@@ -12,87 +10,61 @@ const mime = require('mime-types')
 AWS.config.apiVersions = { s3: '2006-03-01', cloudfront:'2017-03-25' }
 AWS.config.update({ region: process.env.AWS_REGION })
 
-const getFiles = dir =>
-  fs.readdirSync(dir).reduce((arr, file) => {
-    const path = `${dir}/${file}`
-    if (fs.statSync(path).isDirectory()) {
-      return arr.concat(getFiles(path))
-    }
-    return file.startsWith('.') ? [ ...arr ] : [...arr, path]
-  }, [])
-
 const uploadFiles = () => 
   new Promise((resolve, reject) => {
     const s3Client = new AWS.S3()
     const config = { s3Client }
     const client = s3.createClient(config)
-    const Bucket = process.env.AWS_BUCKET
-    const files = getFiles('./public').map(file => ({
-      path: file,
-      key: file.replace(/.*public\//, '').replace(/\/index.html$/, '/')
-    }))
-
-    files.forEach(({ path, key }, i) => {
-      const isStaticFolder = /\/(images|favicon|static)\//g.test(path)
-      const ContentType = mime.lookup(path)
-
-      console.log(path, key)
+    const bucket = process.env.AWS_BUCKET
+    const uploader = client.uploadDir({
+      localDir: 'public',
+      deleteRemoved: true,
+      s3Params: { Bucket: bucket },
       
-      s3Client.putObject({
-        Bucket,
-        Key: key,
-        Body: fs.readFileSync(path),
-        ContentType,
-        ACL: 'public-read',
-        CacheControl: (isStaticFolder 
-          ? 'public,max-age=31536000,immutable' 
-          : 'public,max-age=0,must-revalidate'
+      getS3Params: (filepath, stat, callback) => {
+        const isStaticFolder = /\/(static|images|favicons)\//g.test(filepath)
+        const ContentType = mime.lookup(filepath)
+        
+        callback(null, isStaticFolder 
+          ? { CacheControl: 'public,max-age=31536000,immutable' }
+          : { CacheControl: 'public,max-age=0,must-revalidate', ACL: 'public-read' }
         )
-      }, e => e && console.log(e))
-
-      if (i === (files.length - 1)) {
-        console.log(chalk.green('✔ Files uploaded!'))
-        resolve()
       }
     })
-  })
+    
+    uploader.on('error', err => reject(err))
+    
+    uploader.on('end', () => {
+      console.log(chalk.green('✔ SUCCESS: Files uploaded to production bucket')) 
+      resolve()
+    })
+  })  
 
-const invalidateCache = () => 
+const invalidateCache = id => 
   new Promise((resolve, reject) => {
     const cloudfront = new AWS.CloudFront()
-    const ids = [ 
-      process.env.CLOUDFRONT_DISTRIBUTION_ID_01, 
-      process.env.CLOUDFRONT_DISTRIBUTION_ID_02
-    ]
 
-    ids.forEach((id, i) => {
-      const reference = Date.now()
-      const invalidation = {
-        DistributionId: id,
-        InvalidationBatch: {
-          CallerReference: reference.toString(),
-          Paths: {
-            Quantity: 1,
-            Items: ['/*']
-          }
+    const reference = Date.now()
+    const invalidation = {
+      DistributionId: id,
+      InvalidationBatch: {
+        CallerReference: reference.toString(),
+        Paths: {
+          Quantity: 1,
+          Items: ['/*']
         }
       }
-      
-      cloudfront.createInvalidation(invalidation, err => err && reject(err.message))
-
-      if (i === (ids.length - 1)) {
-        console.log(chalk.green('✔ Cache invalidated!'))
-        resolve()
-      }
+    }
+    
+    cloudfront.createInvalidation(invalidation, err => {
+      if (err) reject(err)
+      console.log(chalk.green(`✔ SUCCESS: Cache of ${id} invalidated`)) 
+      resolve()
     })
   })
 
 uploadFiles()
-  .then(invalidateCache)
+  .then(() => invalidateCache(process.env.CLOUDFRONT_DISTRIBUTION_ID_01))
+  .then(() => invalidateCache(process.env.CLOUDFRONT_DISTRIBUTION_ID_02))
   .then(() => process.exit(0))
-  .catch(err => console.log(chalk.red(`✗ ${err}`)))
-
-//uploadFiles()
-//  //.then(invalidateCache)
-//  
-//  
+  .catch(err => console.log(chalk.red(`✗ ${err.message}`))) 
